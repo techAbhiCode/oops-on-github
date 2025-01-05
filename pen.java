dc46b62863796525d317cbe3ea0fd9021b4cf7b9
Shivam <shivamtech431@gmail.com>
2025-01-05 17:37:04 +0530

    public class pen created to use in OOps file

---
+++ pen.java
@@ -0,0 +1,17 @@
+public class pen {
+    String color;
+    int tip;
+
+    public String getcolor(){
+        return this.color;
+    }
+    public int gettip(){
+        return this.tip;
+    }
+    public void setcolor(String newColor){
+        color = newColor;
+    }
+    public void settip(int newTip){
+        tip = newTip;
+    }
+}
